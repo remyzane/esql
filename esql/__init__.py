2d4b0f20de710d965988468e1e580fa794ea5dc8
Remy Zane <remyzane@icloud.com>
2017-03-10 09:50:15 +0800

    update api

--- esql/__init__.py
+++ esql/__init__.py
@@ -1,5 +1,5 @@
 import os
-from bottle import Bottle, request, static_file
+from bottle import Bottle, request, static_file, redirect
 from esql.utility import request_param
 from esql.utility.configure import Environment
 
@@ -13,6 +13,11 @@ from ql import init, parse
 init(env.config['parser']['optimize'], env.config['parser']['debug'])
 
 
+@app.route('/')
+def index():
+    redirect("/static/esql/index.html")
+
+
 @app.route('/static/<filepath:path>')
 def server_static(filepath):
     return static_file(filepath, root=os.path.join(env.workspace, 'static'))
@@ -22,12 +27,23 @@ def server_static(filepath):
 def execute():
     """ Execute Sql in ES
     """
+    print(request_param('sql'))
     sql = request_param('sql').strip()
+    if sql[-1] != ';':
+        sql += ';'
     query = parse(sql)
     if not query:
         return None
-    dsl = query.dsl()
-    return dsl
+    params = {
+        'search_type': 'query_then_fetch',
+        'index': query._index,
+        'doc_type': query._type,
+        'body': query.dsl(),
+        'ignore_unavailable': True,
+        'request_timeout': 300  # ESql.request_timeout.get('select', ESql.request_timeout_default)
+    }
+    result = env.es.search(**params)
+    return result
 
 
 @app.route('/web', method=('GET', 'POST'))
